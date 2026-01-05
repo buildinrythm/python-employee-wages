9c61d5abcc375d6efd1df625053774877a78675c
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-01-05 09:56:05 +0000

    Converted statement to use f strings

--- wage_calculator.py
+++ wage_calculator.py
@@ -8,7 +8,7 @@ taxRate = 0.00
 employeeName = input("Enter you name")
 hoursWorked = int(input("Enter hours worked"))
 rateOfPay = float(input("Enter rate of pay"))
-taxRate = float(input("enter tax rate"))
+taxRate = float(input("enter tax rate")) / 100
 
 
 # Calculations
@@ -18,11 +18,12 @@ nettPay   = grossPay - taxPaid
 
 #Print emp statement
 print("====== EMPLOYEE STATEMENT ======")
-print("Employee name:", employeeName)
-print("Hours worked", hoursWorked)
-print("Rate of pay: $", rateOfPay)
-print("Tax rate:", taxRate)
-print("Gross pay: $" + str(grossPay))
-print("Tax paid: $)", taxPaid)
-print("Nett pay $", nettPay)
+print(f"Employee name: {employeeName}")
+print(f"Hours worked: {hoursWorked}")
+print(f"Rate of pay: {rateOfPay}$")
+print(f"Tax rate: {taxRate}")
+print(f"Gross pay: {grossPay}$")
+print(f"Tax paid: {taxPaid}$")
+print(f"Nett pay: {nettPay}$")
+print("================================")
 
