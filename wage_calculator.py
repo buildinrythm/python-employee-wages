a4a1b5dc3963073e37c2b4a9ae3ec3d814aa16d2
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-01-05 12:32:06 +0000

    Added bonus calculaton

--- wage_calculator.py
+++ wage_calculator.py
@@ -3,6 +3,7 @@ employeeName = ""
 hoursWorked = 0
 rateOfPay = 0.00
 taxRate = 0.00
+bonus = 0
 
 # Get user input
 employeeName = input("Enter you name")
@@ -16,14 +17,26 @@ grossPay = hoursWorked * rateOfPay
 taxPaid    = grossPay * taxRate		
 nettPay   = grossPay - taxPaid	
 
+if hoursWorked >= 50:
+    bonus = 100
+elif hoursWorked >= 45:
+    bonus = 60
+elif hoursWorked >= 40:
+    bonus = 50
+else:
+    bonus = 0
+
+nettPay += bonus
+
 #Print emp statement
 print("====== EMPLOYEE STATEMENT ======")
-print(f"Employee name: {employeeName}")
-print(f"Hours worked: {hoursWorked}")
-print(f"Rate of pay: ${rateOfPay}")
-print(f"Tax rate: {taxRate:.2f}%")
-print(f"Gross pay: ${grossPay:.2f}")
-print(f"Tax paid: ${taxPaid:.2f}")
-print(f"Nett pay: ${nettPay:.2f}")
+print(f"Employee name:\t {employeeName}")
+print(f"Hours worked:\t {hoursWorked}")
+print(f"Rate of pay:\t ${rateOfPay}")
+print(f"Tax rate:\t {taxRate:.2f}%")
+print(f"Gross pay:\t ${grossPay:.2f}")
+print(f"Tax paid:\t ${taxPaid:.2f}")
+print(f"Nett pay:\t ${nettPay:.2f}")
+print(f"Bonus:\t\t ${bonus}")
 print("================================")
 
